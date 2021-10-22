f1b4a82601d9cb2751fc46dddcd8c2522de2b26e
Peter Zhang <peter@innocraft.com>
2021-10-22 13:40:56 +1300

    add php unit test
    add php unit test

--- plugins/Monolog/tests/Unit/Processor/ExceptionToTextProcessorTest.php
+++ plugins/Monolog/tests/Unit/Processor/ExceptionToTextProcessorTest.php
@@ -167,7 +167,7 @@ test message
 #11 {main}
 EOI;
 
-        $this->assertEquals("adsad", $wholeTrace);
+        $this->assertEquals($this->handleNewerPHPUnitTrace($expected), $wholeTrace);
     }
 
     public function test_getMessageAndWholeBacktrace_printsBacktraceIf_PIWIK_PRINT_ERROR_BACKTRACE_isDefined()
